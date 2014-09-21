4c992014c55671e5cbf42b2043f6a2e8069dcd21
Adrian Böhm <adrian.boehm22@googlemail.com>
2014-09-22 01:36:53 +0200

    controlpanel

---
+++ src/de/_1nulleins0/Parable/ControlPanel.java
@@ -0,0 +1,15 @@
+package de._1nulleins0.Parable;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ControlPanel extends JPanel {
+
+    JButton button;
+
+    public ControlPanel() {
+	button = new JButton("test");
+	this.add(button);
+    }
+
+}
--- src/de/_1nulleins0/Parable/Window.java
+++ src/de/_1nulleins0/Parable/Window.java
@@ -3,17 +3,35 @@ package de._1nulleins0.Parable;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 public class Window {
 
+    ControlPanel cp;
+    JPanel frameholder;
+    JSplitPane splitpane;
+    
     public Window(int w, int h, String title, Main m) {
-
+	cp = new ControlPanel();
+	frameholder = new JPanel();
+	splitpane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+	
+	cp.setPreferredSize(new Dimension(w, 50));
+	cp.setMaximumSize(new Dimension(w, 50));
+	cp.setMinimumSize(new Dimension(w, 50));
+	
 	m.setPreferredSize(new Dimension(w, h));
 	m.setMaximumSize(new Dimension(w, h));
 	m.setMinimumSize(new Dimension(w, h));
-
+	
+	frameholder.add(m);
+	
+	splitpane.setBottomComponent(cp);
+        splitpane.setTopComponent(frameholder);
+        
 	JFrame frame = new JFrame(title);
-	frame.add(m);
+	frame.add(splitpane);
 	frame.pack();
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setResizable(false);
